8dd75bd265d6c2f97d53f2e725381588a23b1cef
markojot <90350349+markojot@users.noreply.github.com>
2022-06-01 14:52:02 -0400

    adding main and variables

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "fullcompanyname" {
+  default = "Arkansas Blue Cross Blue Shield"
+}
+variable "shortcompanyname" {
+  default = "ABCBS"
+}
+variable "CompanyManagementGroupName" {
+  default = "Tenant Root Group"
+}
+variable "Sandbox" {
+    default = "Sandbox"
+ }
+ variable "platform" {
+     default = "platform"
+ } 
+ variable "production" {
+     default = "production"
+ } 
+ variable "staging" {
+     default = "staging"
+ } 
+  variable "development" {
+     default = "development"
+ }
+  variable "production-hr" {
+     default = "prod-hr"
+ }
+   variable "production-marketing" {
+     default = "prod-marketing"
+ }
+   variable "IT" {
+     default = "IT"
+ }
+    variable "shared" {
+     default = "shared"
+ }
